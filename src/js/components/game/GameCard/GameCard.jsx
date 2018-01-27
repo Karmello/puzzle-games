1e9a77a35075a395e42327efa51e5d476c1c24e4
Noga Kamil <kamil.noga@jcommerce.pl>
2018-01-27 19:08:43 +0100

    options component loading dynamically inside GameCard now

--- src/js/components/game/GameCard/GameCard.jsx
+++ src/js/components/game/GameCard/GameCard.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, Typography } from 'material-ui';
 import { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 
-import { BossPuzzleOptions } from 'js/components/game';
 import './GameCard.css';
 
 
-export default (props) => (
-  <div className='GameCard'>
-    <Card>
-      <CardMedia
-        style={{ height: '250px' }}
-        image={`${process.env.REACT_APP_S3_BUCKET}/${props.gameData.id}/logo.jpg`}
-        title={props.gameData.name}
-      />
-      <CardContent>
-        <Typography type='headline' component='h2'>{props.gameData.name}</Typography>
-        <Typography component='p'>{props.gameData.description}</Typography>
-        <div className='GameCard-options'>
-          {props.gameData.id === 'BossPuzzle' &&
-          <BossPuzzleOptions
-            options={props.gameOptions}
-            onValueChangeCb={(options) => { props.onGameOptionsChange(props.gameData.id, options); }}
-          />}
-        </div>
-      </CardContent>
-      <CardActions className='GameCard-actions'>
-        <div>
-          <Button
-            color='primary'
-            component={Link}
-            to={`/games/${props.gameData.id}`}
-          >Play</Button>
-        </div>
-      </CardActions>
-    </Card>
-  </div>
-);
+export default class GameCard extends Component {
+
+  render() {
+
+    const { gameData, gameOptions, onGameOptionsChange } = this.props;
+    const Options = require(`js/components/game/gameOptions/${gameData.id}Options/${gameData.id}Options`).default;
+
+    return (
+      <div className='GameCard'>
+        <Card>
+          <CardMedia
+            style={{ height: '250px' }}
+            image={`${process.env.REACT_APP_S3_BUCKET}/${gameData.id}/logo.jpg`}
+            title={gameData.name}
+          />
+          <CardContent>
+            <Typography type='headline' component='h2'>{gameData.name}</Typography>
+            <Typography component='p'>{gameData.description}</Typography>
+            <div className='GameCard-options'>
+              <Options
+                options={gameOptions}
+                onValueChangeCb={options => onGameOptionsChange(gameData.id, options)}
+              />
+            </div>
+          </CardContent>
+          <CardActions className='GameCard-actions'>
+            <div>
+              <Button
+                color='primary'
+                component={Link}
+                to={`/games/${gameData.id}`}
+              >Play</Button>
+            </div>
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
