996fc412ea53ccf3e1b59f34fe701742c2303dff
Kamil Noga <nogakamil@vp.pl>
2018-03-16 13:48:11 +0100

    #51: loader stops after game init

--- src/js/engines/EightQueens/EightQueens.jsx
+++ src/js/engines/EightQueens/EightQueens.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './EightQueens.css';
 
 
-export default class EightQueens extends Component {
+class EightQueens extends Component {
+
+  componentDidMount() {
+    this.props.onFinishInit();
+  }
 
   render() {
-    return <div className='EightQueens'>Eight Queens</div>
+    return (
+      <div className='EightQueens'>Eight Queens</div>
+    );
   }
 }
+
+EightQueens.propTypes = {
+  onFinishInit: PropTypes.func.isRequired,
+  onMakeMove: PropTypes.func.isRequired,
+  onBeenSolved: PropTypes.func.isRequired,
+  restarting: PropTypes.bool.isRequired
+};
+
+export default connect(store => ({
+  game: store.pages.gamePage,
+  eightQueensEngine: store.engines.EightQueens
+}))(EightQueens);
