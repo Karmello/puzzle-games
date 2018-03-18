4adb901092255074e008092927f71f81f5f1e008
Kamil Noga <nogakamil@vp.pl>
2018-03-18 15:29:30 +0100

    #51: implemented checkIfSolved method

--- src/js/engines/EightQueens/EightQueens.jsx
+++ src/js/engines/EightQueens/EightQueens.jsx
@@ -7,11 +7,13 @@ import { Game, GridGameBoard } from 'js/game';
 import { initFrame, moveQueen, resetFrame } from 'js/engines/EightQueens/eightQueens.actions';
 
 
-const squareSize = 75;
-
 class EightQueens extends Game {
 
-  static dimension = 8;
+  constructor(props) {
+    super(props);
+    this.dimension = 8;
+    this.squareSize = 75;
+  }
 
   componentWillUnmount() {
     this.props.dispatch(resetFrame());
@@ -21,8 +23,8 @@ class EightQueens extends Game {
     const { eightQueensEngine } = this.props;
     return (
       <GridGameBoard
-        dimension={EightQueens.dimension}
-        squareSize={squareSize}
+        dimension={this.dimension}
+        squareSize={this.squareSize}
         Square={() => <Button disableRipple style={this.getBtnStyle()}> </Button>}
         draggable={true}
         gridData={eightQueensEngine.queens}
@@ -34,8 +36,8 @@ class EightQueens extends Game {
   startNew() {
     return new Promise(resolve => {
       this.loadImg('EightQueens/queen.png').then(() => {
-        const queens = Array.from({ length: EightQueens.dimension ** 2 }, (v, k) => {
-          const coords = GridGameBoard.indexToCoords(k, EightQueens.dimension);
+        const queens = Array.from({ length: this.dimension ** 2 }, (v, k) => {
+          const coords = GridGameBoard.indexToCoords(k, this.dimension);
           return coords.x === coords.y;
         });
         this.props.dispatch(initFrame(queens));
@@ -50,17 +52,66 @@ class EightQueens extends Game {
   }
 
   checkIfSolved() {
-    return new Promise(resolve => resolve(false));
+    
+    return new Promise(resolve => {
+
+      const { coordsToIndex, indexToCoords } = GridGameBoard;
+      const dimension = this.dimension;
+      const { eightQueensEngine } = this.props;
+      const queens = eightQueensEngine.queens;
+      const qIndxs = [];
+
+      for (const key in queens) {
+        if (queens[key]) {
+          qIndxs.push(Number(key));
+        }
+      }
+
+      for (const q of qIndxs) {
+        
+        const qCrds = indexToCoords(q, dimension);
+        
+        for (let x = 0; x < dimension; x++) {
+          if (x !== qCrds.x && queens[coordsToIndex({ x, y: qCrds.y }, dimension)]) {
+            return resolve(false);
+          }
+        }
+        
+        for (let y = 0; y < dimension; y++) {
+          if (y !== qCrds.y && queens[coordsToIndex({ x: qCrds.x, y }, dimension)]) {
+            return resolve(false);
+          }
+        }
+        
+        for (let z1 = 0; z1 < dimension - Math.abs(qCrds.x - qCrds.y); z1++) {
+          const { x, y } = qCrds;
+          const i = (((y - Math.min(x, y)) + z1) * dimension) + (x - Math.min(x, y) + z1);
+          if (i !== q && queens[i]) {
+            return resolve(false);
+          }
+        }
+
+        for (let z2 = 0; z2 < dimension - Math.abs(dimension - qCrds.x - qCrds.y - 1); z2++) {
+          const { x, y } = qCrds;
+          const i = (((y + Math.min(dimension - 1 - y, x)) - z2) * dimension) + (x - Math.min(dimension - 1 - y, x) + z2);
+          if (i !== q && queens[i]) {
+            return resolve(false);
+          }
+        }
+      }
+
+      resolve(true);
+    });
   }
 
   getBtnStyle() {
     return  {
-      minWidth: `${squareSize}px`,
-      height: `${squareSize}px`,
-      border: '2px solid',
+      minWidth: `${this.squareSize}px`,
+      height: `${this.squareSize}px`,
+      border: '1px solid gray',
       borderRadius: '0px',
       backgroundImage: `url(${process.env.REACT_APP_S3_BUCKET}/EightQueens/queen.png)`,
-      backgroundSize: `${squareSize-4}px ${squareSize-4}px`,
+      backgroundSize: `${this.squareSize-2}px ${this.squareSize-2}px`,
       backgroundColor: 'white'
     }
   }
