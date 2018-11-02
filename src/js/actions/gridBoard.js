e39e9691a9cd20436fb6d0a7135b427b53d71d62
Karmello <nogakamil@vp.pl>
2018-11-02 22:44:03 +0100

    #116 - decided not to introduce gridBoard actions and reducer files

--- src/js/actions/gridBoard.js
+++
@@ -1,7 +0,0 @@
-// @flow
-export const GRID_BOARD_INIT = 'GRID_BOARD_INIT';
-
-export const initGridBoard = () => ({
-  type: GRID_BOARD_INIT,
-  payload: {}
-});
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -15,8 +15,6 @@ import eightQueensReducer from './eightQueens';
 import knightsTourReducer from './knightsTour';
 import sudokuReducer from './sudoku';
 
-import gridBoardReducer from './gridBoard';
-
 import {
   API_MAKE_AUTH_REQUEST,
   API_FETCH_USERS,
@@ -50,6 +48,5 @@ export default combineReducers({
     'knights-tour': knightsTourReducer,
     'sudoku': sudokuReducer
   }),
-  gridBoard: gridBoardReducer,
   form: formReducer
 });
